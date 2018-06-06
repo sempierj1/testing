84b87f992afcf4fdaea7fa6c6ccf931e91cbdbad
sempierj1 <36780054+sempierj1@users.noreply.github.com>
2018-06-06 12:13:36 -0400

    Update Dashboard.js

--- Dashboard.js
+++ Dashboard.js
@@ -1,57 +1,7 @@
-import React, { Component } from "react";
-import { AuthUser } from "./AuthContext";
-import { Doughnut } from "react-chartjs-2";
-import firebase from "./firebase";
-
-class Dashboard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      singleCount: 0,
-      familyCount: 0,
-      seniorCount: 0,
-      seniorCoupleCount: 0,
-      basicCount: 0,
-      lifetimeCount: 0,
-      getType: false
-    };
-    this.memberTypes = this.memberTypes.bind(this);
-    this.getTypeData = this.getTypeData.bind(this);
-  }
-
-  getTypeData = () => {
-    if (
-      this.state.singleCount === 0 &&
-      this.state.familyCount === 0 &&
-      this.state.seniorCount === 0 &&
-      this.state.getType === false
-    ) {
-      return this.memberTypes();
-    } else {
-      return {
-        datasets: [
-          {
-            data: [
-              this.state.singleCount,
-              this.state.familyCount,
-              this.state.seniorCount
-            ]
-          }
-        ],
-        labels: ["Single Membership", "Family Membership", "Senior Membership"]
-      };
-    }
-  };
-
-  logout = () => {
-    console.log("Made it");
-    window.location = "/";
-  };
-
-  memberTypes = () => {
+  memberTypes = async() => {
 
     var counts = new Map();
-    
+
     counts.set("Family Membership", 0);
     counts.set("Single Membership", 0);
     counts.set("Senior Membership", 0);
@@ -61,20 +11,55 @@ class Dashboard extends Component {
 
     var ref = firebase.database().ref("badges");
 
+    const setAndReturn = () => {
+      this.setState({
+        getType: true,
+        singleCount: counts.get("Single Membership"),
+        seniorCount: counts.get("Senior Membership"),
+        familyCount: counts.get("Family Membership"),
+        lifetimeCount: counts.get("Lifetime Member"),
+        seniorCoupleCount: counts.get("Senior Couple"),
+        basicCount: counts.get("Basic Membership")
+      });
+      return {
+        datasets: [
+          {
+            data: [
+              this.state.singleCount,
+              this.state.familyCount,
+              this.state.seniorCount,
+              this.state.lifetimeCount,
+              this.state.seniorCoupleCount,
+              this.state.basicCount
+            ]
+          }
+        ],
+        labels: ["Single", "Family", "Senior", "Lifetime", "Senior Couple", "Basic"]
+      };
 
-    const getTypes = (value, key, map) => {
+    }
+
+    var itemsProcessed = 0;
+
+    const getTypes = async (value, key, map) => {
       var newref = firebase.database().ref("users/" + value + "/type");
-      newref.once("value", data => {
+      await newref.once("value", data => {
         var tempType = data.val();
         var tempCount = counts.get(tempType);
         tempCount++;
         counts.set(tempType, tempCount);
+        itemsProcessed++;
+      }).then((res) =>{
+        if(itemsProcessed === map.size)
+        {
+          setAndReturn();
+        }
       });
+      
 
     };
 
-    console.log(counts);
-    ref.once("value", data => {
+    await ref.once("value", async(data) => {
       var temp = data.val();
       var map = new Map();
       Object.keys(temp).forEach(key => {
@@ -83,50 +68,6 @@ class Dashboard extends Component {
       map.forEach(getTypes);
     });
 
-    this.setState({
-      getType: true,
-      singleCount: counts.get("Single Membership"),
-      seniorCount: counts.get("Senior Membership"),
-      familyCount: counts.get("Family Membership")
-    });
+  
 
-    return {
-      datasets: [
-        {
-          data: [
-            this.state.singleCount,
-            this.state.familyCount,
-            this.state.seniorCount
-          ]
-        }
-      ],
-      labels: ["Single Membership", "Family Membership", "Senior Membership"]
-    };
-  };
-
-  render() {
-    /*return (
-      <AuthUser>
-        {({ isAuth, logout }) => (
-          <div>
-            <button
-              type="submit"
-              onClick={() => {
-                this.logout();
-              }}
-            >
-              Log Out
-            </button>
-          </div>
-        )}
-      </AuthUser>
-    );*/
-    return (
-      <div>
-        <Doughnut data={this.getTypeData} />
-      </div>
-    );
   }
-}
-
-export default Dashboard;
